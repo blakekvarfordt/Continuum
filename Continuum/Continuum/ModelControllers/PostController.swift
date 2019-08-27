2de1b18781bdb3277ebcf9e1bde9ebec5968ef32
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-27 14:09:11 -0600

    Sets up PostController CreatePost function

---
+++ Continuum/Continuum/ModelControllers/PostController.swift
@@ -0,0 +1,27 @@
+//
+//  PostController.swift
+//  Continuum
+//
+//  Created by Blake kvarfordt on 8/27/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIImage
+
+class PostController {
+    
+    static let shared = PostController()
+    
+    var posts: [Post] = []
+    
+    func addComment(text: String, post: Post, completion: @escaping (Comment) -> Void) {
+        
+    }
+    
+    func createPostWith(photo: UIImage, caption: String, completion: @escaping (Post?) -> Void) {
+        let post = Post(photo: photo, caption: caption)
+        self.posts.append(post)
+    }
+        
+}
