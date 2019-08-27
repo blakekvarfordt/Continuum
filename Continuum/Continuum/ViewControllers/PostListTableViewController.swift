bafefde1f4143991e9ada621377b2d164b123087
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-27 14:16:54 -0600

    Sets up PostListViewController with Segue to PostDetailViewController

--- Continuum/Continuum/ViewControllers/PostListTableViewController.swift
+++ Continuum/Continuum/ViewControllers/PostListTableViewController.swift
@@ -16,31 +16,31 @@ class PostListTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return PostController.shared.posts.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as? PostTableViewCell else { return UITableViewCell()}
 
+        let post = PostController.shared.posts[indexPath.row]
         
+        cell.postImageView.image = post.photo
+        cell.captionLabel.text = post.caption
+        cell.numberOfCommentsLabel.text = "\(post.comments.count)"
 
         return cell
     }
     
-
-   
-    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       
+        if segue.identifier == "ToPostDetailTableviewController" {
+            guard let index = tableView.indexPathForSelectedRow, let destination = segue.destination as? PostDetailTableViewController else { return }
+            let post = PostController.shared.posts[index.row]
+            destination.post = post
+        }
     }
     
 
