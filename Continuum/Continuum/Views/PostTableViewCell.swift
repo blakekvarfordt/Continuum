5f1353d47aeaa57acfa9ea7aa1d34d50b56197c8
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-27 14:09:58 -0600

    Sets up PostTableViewCell and Cell in storyBoards

---
+++ Continuum/Continuum/Views/PostTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostTableViewCell.swift
+//  Continuum
+//
+//  Created by Blake kvarfordt on 8/27/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var numberOfCommentsLabel: UILabel!
+    
+    
+    var post: Post? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    func updateViews() {
+        guard let post = post else { return }
+        postImageView.image = post.photo
+        captionLabel.text = post.caption
+        numberOfCommentsLabel.text = "\(post.comments.count)"
+    }
+
+}
