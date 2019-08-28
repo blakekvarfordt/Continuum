5dc43867de57495eecced78079ab90af28f9772c
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-28 13:26:04 -0600

    makes search term lowercased

--- Continuum/Continuum/Models/Comment.swift
+++ Continuum/Continuum/Models/Comment.swift
@@ -23,7 +23,7 @@ class Comment {
 
 extension Comment: SearchableRecord {
     func matches(searchTerm: String) -> Bool {
-        return text.contains(searchTerm) ? true : false
+        return text.contains(searchTerm.lowercased()) ? true : false
     }
     
     
